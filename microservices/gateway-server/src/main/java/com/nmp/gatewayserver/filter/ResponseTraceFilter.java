0bef9206a46b96bcda3ea69e10b020b571c048a6
Nisarg Patel <patelnisargm@gmail.com>
2024-02-10 12:08:26 -0500

    OAuth2 changes

--- microservices/gateway-server/src/main/java/com/nmp/gatewayserver/filter/ResponseTraceFilter.java
+++ microservices/gateway-server/src/main/java/com/nmp/gatewayserver/filter/ResponseTraceFilter.java
@@ -24,14 +24,11 @@ public class ResponseTraceFilter {
 
     @Bean
     public GlobalFilter postGlobalFilter() {
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
-            }));
-        };
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+            String correlationId = filterUtility.getCorrelationId(requestHeaders);
+            logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+            exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+        }));
     }
-
 }
